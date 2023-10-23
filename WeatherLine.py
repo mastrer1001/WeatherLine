577d7fcbd2d7a214220b39131b268eb441c86811
mastrer1001 <83538515+mastrer1001@users.noreply.github.com>
2023-10-23 02:13:44 +0200

    Add files via upload

---
+++ WeatherLine.py
@@ -0,0 +1,173 @@
+import sys
+import requests
+import json
+
+CONFIG_FILE = "config.json"
+
+def get_default_unit():
+    try:
+        with open(CONFIG_FILE, "r") as config_file:
+            config = json.load(config_file)
+            return config.get("default_unit")
+    except FileNotFoundError:
+        create_default_config()
+        sys.exit(1)
+
+def get_default_city():
+    try:
+        with open(CONFIG_FILE, "r") as config_file:
+            config = json.load(config_file)
+            return config.get("default_city")
+    except FileNotFoundError:
+        create_default_config()
+        sys.exit(1)
+
+def get_api_key():
+    try:
+        with open(CONFIG_FILE, "r") as config_file:
+            config = json.load(config_file)
+            return config.get("api_key")
+    except FileNotFoundError:
+        create_default_config()
+        sys.exit(1)
+
+def create_default_config():
+    try:
+        with open(CONFIG_FILE, "r") as config_file:
+            config = json.load(config_file)
+    except FileNotFoundError:
+        config = {}
+
+    if "api_key" not in config:
+        config["api_key"] = "YOUR_INITIAL_API_KEY" # Set your desired API key here
+    
+    if "default_city" not in config:
+        config["default_city"] = ""  # Set your desired default city here
+
+    if "default_unit" not in config:
+        config["default_unit"] = "standard"  # Set your desired default unit here
+
+    with open(CONFIG_FILE, "w") as config_file:
+        json.dump(config, config_file)
+
+def update_default_city(default_city):
+    try:
+        with open(CONFIG_FILE, "r") as config_file:
+            config = json.load(config_file)
+    except FileNotFoundError:
+        config = {}
+
+    config["default_city"] = default_city
+
+    with open(CONFIG_FILE, "w") as config_file:
+        json.dump(config, config_file)
+
+def update_default_unit(default_unit):
+    try:
+        with open(CONFIG_FILE, "r") as config_file:
+            config = json.load(config_file)
+    except FileNotFoundError:
+        config = {}
+
+    config["default_unit"] = default_unit
+
+    with open(CONFIG_FILE, "w") as config_file:
+        json.dump(config, config_file)
+
+def update_api_key(api_key):
+    try:
+        with open(CONFIG_FILE, "r") as config_file:
+            config = json.load(config_file)
+    except FileNotFoundError:
+        config = {}
+
+    config["api_key"] = api_key
+
+    with open(CONFIG_FILE, "w") as config_file:
+        json.dump(config, config_file)
+def get_weather(city, api_key, unit = "standart"):
+    base_url = 'https://api.openweathermap.org/data/2.5/weather'
+
+    params = {
+        'q': city,
+        'appid': api_key,
+        'units': unit
+    }
+
+    response = requests.get(base_url, params=params)
+    data = response.json()
+
+    if response.status_code == 200:
+        weather_description = data['weather'][0]['description']
+        temperature = data['main']['temp']
+
+        # Extract the unit from the API response
+        if unit == "imperial":
+            unit_symbol = "°F"
+        elif unit == "metric":
+            unit_symbol = "°C"
+        else:
+            unit_symbol = "K"
+
+        # Format the temperature with the appropriate unit
+        temperature_with_unit = f"Temperature: {temperature}{unit_symbol}"
+        print(f"Weather in {city}: {weather_description.capitalize()}")
+        print(temperature_with_unit)
+    else:
+        print(f"Failed to retrieve weather data for {city}")
+        print(data)
+
+if __name__ == "__main__":
+    # if len(sys.argv) < 3:
+    #     print("Usage: python weather.py -q <city> [-k <api-key>] [-d <default-city>] [-u <unit>]")        
+    #     sys.exit(1)
+
+    args = {}
+    i = 1
+
+    while i < len(sys.argv):
+        if sys.argv[i] == "-h":
+            print("Usage: python weather.py -q <city> [-k <api-key>] [-u <unit>] [-d <default-city>]")
+            print("Options:")
+            print("  -q <city>        Query the weather for the specified city.")
+            print("  -k <api-key>     Set your OpenWeatherMap API key.")
+            print("  -u <unit>        Set the preferred unit (metric, imperial, standard). Default is metric.")
+            print("  -d <default-city> Set the default city for weather queries.")
+            sys.exit(0)
+        
+        if sys.argv[i] in ["-q", "-k", "-u", "-d"]:
+            if i + 1 < len(sys.argv):
+                args[sys.argv[i]] = sys.argv[i + 1]
+                i += 2
+            else:
+                print(f"Please provide a value after '{sys.argv[i]}'.")
+                sys.exit(1)
+        else:
+            print(f"Unrecognized option: {sys.argv[i]}")
+            sys.exit(1)
+
+    city = args.get("-q")
+    api_key = args.get("-k")
+    unit = args.get("-u")
+
+    if api_key:
+        update_api_key(api_key)
+
+    if "-u" in args:
+        update_default_unit(unit)
+    
+    if "-d" in args:
+        default_city = args["-d"]
+        update_default_city(default_city)
+
+    if not city:
+        city=get_default_city()
+
+    if not api_key:
+        api_key=get_api_key()
+    
+    if not unit:
+        unit=get_default_unit()
+    
+    if city:
+        get_weather(city, api_key, unit)
---
+++ readme.md
@@ -0,0 +1,81 @@
+Certainly! Here's an example of a README file that explains what your program does and how to use it:
+
+markdown
+
+# Weather Query Program
+
+The Weather Query Program is a command-line tool that allows you to retrieve current weather information for a specified city using the OpenWeatherMap API. It also provides options to set a default city, API key, and preferred unit for weather queries.
+
+## Features
+
+- Query the current weather for a specific city.
+- Set or update your OpenWeatherMap API key.
+- Specify a default city for weather queries.
+- Set the preferred unit for temperature (metric, imperial, or standard).
+
+## Prerequisites
+
+Before using the Weather Query Program, you need to obtain an OpenWeatherMap API key. You can sign up for a free API key on the [OpenWeatherMap website](https://openweathermap.org/api).
+
+## Installation
+
+1. Clone this repository to your local machine using `git clone`.
+2. Install the required Python packages using `pip`:
+
+pip install requests
+
+arduino
+
+
+## Usage
+
+To use the Weather Query Program, open a command-line terminal and run the program with the following options:
+
+python weather.py -q <city> [-k <api-key>] [-d <default-city>] [-u <unit>]
+
+markdown
+
+
+### Options
+
+- `-q <city>`: Query the weather for the specified city.
+- `-k <api-key>`: Set your OpenWeatherMap API key.
+- `-d <default-city>`: Set the default city for weather queries.
+- `-u <unit>`: Set the preferred unit for temperature (metric, imperial, or standard). Default is metric.
+
+### Examples
+
+1. Query the weather for a specific city:
+   
+
+python weather.py -q Berlin
+
+vbnet
+
+
+2. Set your API key:
+
+python weather.py -k YOUR_API_KEY
+
+javascript
+
+
+3. Set a default city for weather queries:
+
+python weather.py -d Paris
+
+vbnet
+
+
+4. Set the preferred temperature unit to imperial (Fahrenheit):
+
+python weather.py -u imperial
+
+perl
+
+
+### Help
+
+To see the available options and their descriptions, you can use the `-h` or `--help` option:
+
+python weather.py -h
